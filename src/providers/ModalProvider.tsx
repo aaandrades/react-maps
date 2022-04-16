93514906d08a25886eace09a2d61a7bd846df7ce
Andres Andrade <aaandrades@outlook.com>
2022-04-16 18:29:53 -0500

    :sparkles: Added modal provider to help information

--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -20,8 +20,9 @@ class Context {
     const result = this.strategy.getModalInfo();
     Swal.fire({
       title: result.title,
-      text: "Do you want to continue",
-      confirmButtonText: "Cool",
+      text: result.text,
+      confirmButtonText: "Got it!",
+      confirmButtonColor: "#0082c9",
     });
   }
 }
@@ -30,30 +31,42 @@ class Polygon implements Strategy {
   public getModalInfo() {
     return {
       title: "Search by Polygon",
-      description: "Search points drawing a polygon ",
+      text: "Search points drawing a polygon over the map, for example, draw a square to select all points inside the block.",
     };
   }
 }
 
 class Location implements Strategy {
   public getModalInfo() {
-    return {};
+    return {
+      title: "Search by Location",
+      text: "Enable your location in the browser (desktop or mobile) and select a range of meters which you want to find points",
+    };
   }
 }
 
 class Directions implements Strategy {
   public getModalInfo() {
-    return {};
+    return {
+      title: "Directions",
+      text: "Find the directions between your location and the selected point",
+    };
   }
 }
 class Creation implements Strategy {
   public getModalInfo() {
-    return {};
+    return {
+      title: "Creation",
+      text: "Create your own point and add your custom info, but... we haven't implemented a backend :( , so your points will be available just in your session :)",
+    };
   }
 }
 
 interface Strategy {
-  getModalInfo(): any;
+  getModalInfo(): {
+    title: string;
+    text: string;
+  };
 }
 
 export { Polygon, Location, Directions, Creation, Context };
